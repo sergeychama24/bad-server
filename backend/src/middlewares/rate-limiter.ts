6d50638be0f766748cf30299a1870108c01fb205
Sergey Chama <sergeychama24@gmail.com>
2025-01-31 05:42:41 -0800

    fix: fix rate limit value

--- backend/src/middlewares/rate-limiter.ts
+++ backend/src/middlewares/rate-limiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 100,
+    limit: 50,
     standardHeaders: true,
     legacyHeaders: false,
     message: 'Слишком много запросов',
